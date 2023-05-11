24e4861abb3ca0b8c42101077fd9dc20175afd30
Marco Capone <118698117+marcocapone02@users.noreply.github.com>
2023-05-11 22:53:11 +0200

    Film & Series

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,20 @@ import axios, { Axios } from 'axios';
 import { store } from './store';
 import Search from './components/Search.vue';
 import FindFilm from './components/FindFilm.vue';
-export default {
 
+export default {
   name: 'App',
   components:{
     Search,
     FindFilm
   },
-
   data() {
     return {
-      store      
+      store  
     }
   },
-
-  created(){
-  },
-
   methods: {
-    SearchFilm(){
+    SearchApi(){
       axios.all([
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=f65ad0c55ac9547d453d5b83f9f3f576&language=it_IT&query=${store.FilmName}`),
         axios.get(`https://api.themoviedb.org/3/search/tv?api_key=f65ad0c55ac9547d453d5b83f9f3f576&language=it_IT&query=${store.FilmName}`)
@@ -29,22 +24,20 @@ export default {
       .then(axios.spread((res1, res2) =>{
         const films_found = res1.data.results;
         const tv_found = res2.data.results;
-        const merged = [...tv_found, ...films_found];
-        store.arrayFoundAll = merged
-        console.log(store.arrayFoundAll)
+        
+        store.arrayFindFilm = films_found;
+        store.arrayFoundTv = tv_found;
       }))
     }
-  },
+  }
 }
 </script>
 
 <template>
-
   <div id="boolflix" class="overflow-auto">
-    <Search @searchFilm="SearchFilm()"/>
+    <Search @searchFilm="SearchApi()"/>
     <FindFilm/>
   </div>
-
 </template>
 
 <style>
